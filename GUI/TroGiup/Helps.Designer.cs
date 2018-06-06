6587db7733af2c3e53d0d0424cc6fffaee1d14f8
Quang Nghia <quangnghiauit@gmail.com>
2018-06-07 01:12:38 +0700

    updateHelp

--- GUI/TroGiup/Helps.Designer.cs
+++ GUI/TroGiup/Helps.Designer.cs
@@ -47,10 +47,10 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(209, 30);
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(134, 30);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(414, 24);
+			this.label1.Size = new System.Drawing.Size(532, 31);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Hướng dẫn sử dụng phần mềm quản lý gara oto";
 			// 
@@ -65,20 +65,20 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label2.Location = new System.Drawing.Point(102, 108);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(103, 24);
+			this.label2.Size = new System.Drawing.Size(98, 23);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Chức năng";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label3.Location = new System.Drawing.Point(425, 108);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(105, 24);
+			this.label3.Size = new System.Drawing.Size(101, 23);
 			this.label3.TabIndex = 4;
 			this.label3.Text = "Hướng dẫn";
 			// 
