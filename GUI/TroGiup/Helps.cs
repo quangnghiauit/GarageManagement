f5e44b1baa937ca82d7038e446c3261353e3d0c8
Uyen Quang Bui Tran <uyenquangbuitran@gmail.com>
2018-06-09 17:22:30 +0700

    updateHelp
    - Thêm scrollBar cho tbHelp.

--- GUI/TroGiup/Helps.cs
+++ GUI/TroGiup/Helps.cs
@@ -17,6 +17,7 @@ namespace GUI
 		{
 			InitializeComponent();
 			LoadTreeView();
+            tbHelp.ScrollBars = ScrollBars.Vertical;
 		}
 
 		private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
