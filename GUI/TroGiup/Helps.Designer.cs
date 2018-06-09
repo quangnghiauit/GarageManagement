05723113764ee58a993fefb2612a68d9e72aa351
Uyen Quang Bui Tran <uyenquangbuitran@gmail.com>
2018-06-09 14:17:14 +0700

    updateHelp
    - Cập nhật phần trợ giúp cho chức năng giao dịch và báo cáo.

--- GUI/TroGiup/Helps.Designer.cs
+++ GUI/TroGiup/Helps.Designer.cs
@@ -28,75 +28,77 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.tvHelp = new System.Windows.Forms.TreeView();
-			this.label1 = new System.Windows.Forms.Label();
-			this.tbHelp = new System.Windows.Forms.TextBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// tvHelp
-			// 
-			this.tvHelp.Location = new System.Drawing.Point(44, 157);
-			this.tvHelp.Name = "tvHelp";
-			this.tvHelp.Size = new System.Drawing.Size(181, 298);
-			this.tvHelp.TabIndex = 0;
-			this.tvHelp.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
-			this.tvHelp.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tvHelp_MouseDoubleClick);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(134, 30);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(532, 31);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Hướng dẫn sử dụng phần mềm quản lý gara oto";
-			// 
-			// tbHelp
-			// 
-			this.tbHelp.Location = new System.Drawing.Point(276, 157);
-			this.tbHelp.Multiline = true;
-			this.tbHelp.Name = "tbHelp";
-			this.tbHelp.Size = new System.Drawing.Size(461, 293);
-			this.tbHelp.TabIndex = 2;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(102, 108);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(98, 23);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Chức năng";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(425, 108);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(101, 23);
-			this.label3.TabIndex = 4;
-			this.label3.Text = "Hướng dẫn";
-			// 
-			// fHelps
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(850, 540);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.tbHelp);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.tvHelp);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "fHelps";
-			this.Text = "Helps";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.tvHelp = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHelp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tvHelp
+            // 
+            this.tvHelp.Location = new System.Drawing.Point(44, 157);
+            this.tvHelp.Name = "tvHelp";
+            this.tvHelp.Size = new System.Drawing.Size(181, 298);
+            this.tvHelp.TabIndex = 0;
+            this.tvHelp.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.tvHelp.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tvHelp_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(532, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hướng dẫn sử dụng phần mềm quản lý gara oto";
+            // 
+            // tbHelp
+            // 
+            this.tbHelp.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbHelp.Location = new System.Drawing.Point(276, 157);
+            this.tbHelp.Multiline = true;
+            this.tbHelp.Name = "tbHelp";
+            this.tbHelp.ReadOnly = true;
+            this.tbHelp.Size = new System.Drawing.Size(461, 293);
+            this.tbHelp.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(102, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chức năng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(425, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hướng dẫn";
+            // 
+            // fHelps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 540);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbHelp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tvHelp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fHelps";
+            this.Text = "Helps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- GUI/TroGiup/Helps.cs
+++ GUI/TroGiup/Helps.cs
@@ -34,25 +34,69 @@ namespace GUI
 			{
 				tbHelp.Text = "Hướng dẫn quản lý vật tư.";
 			}
-			if (selectedNodeText == "Tiếp nhận bảo trì xe")
+            if (selectedNodeText == "Giao dịch")
+            {
+                tbHelp.Text = "\tChức năng giao dịch gồm 3 chức năng nhỏ:\r\n"
+                    + "\t- Tiếp nhận bảo trì xe: Ghi nhận khi xe được tiếp nhận bảo trì ở gara.\r\n"
+                    + "\t- Lập phiếu sửa chữa: Ghi nhận chi tiết về các công việc và các vật tư sử dụng trong quá trình sửa chữa xe.\r\n"
+                    + "\t- Lập phiếu thu tiền: Ghi nhận các thông tin khi khách trả tiền.";
+
+            }
+            if (selectedNodeText == "Tiếp nhận bảo trì xe")
 			{
-				tbHelp.Text = "Hướng dẫn tiếp nhận bảo trì xe.";
-			}
+                tbHelp.Text = "\tTên Chủ Xe, Biển Số và Hiệu Xe sẽ được lấy từ dữ liệu đã nhập ở chức năng Quản Lý Khách Hàng và Tra Cứu Xe.\r\n"
+                    + "\tĐịa Chỉ và Số Điện Thoại của khách hàng là chuỗi kí tự được nhập vào.\r\n"
+                    + "\tSau khi nhập đầy đủ các thông tin, nhấn nút tiếp nhận sẽ tiến hành nhập dữ liệu vào.\r\n"
+                    + "\tNút Lịch Sử Tiếp Nhập sẽ đưa ra danh sách các xe đã được tiếp nhận ở bảng bên dưới.\r\n\r\n";
+
+            }
 			if (selectedNodeText == "Lập phiếu sửa chữa")
 			{
-				tbHelp.Text = "Hướng dẫn lập phiếu sữa chữa.";
+				tbHelp.Text = "\tKhung Thao Tác bên phải dùng để nhập từng công việc của Phiếu Sửa Chữa, trong đó:\r\n"
+                    + "\t- Nội dung: Chuỗi kí tự ghi lại nội dung công việc.\r\n"
+                    + "\t- Vật tư phụ tùng: Dữ liệu được lấy từ chức năng Quản Lý Vật Tư.\r\n"
+                    + "\t- Số lượng: Số vật tư phụ tùng đã sử dụng.\r\n"
+                    + "\t- Đơn giá: Giá tiền của một vật tư phụ tùng đã sử dụng.\r\n"
+                    + "\t- Tiền công: Dữ liệu được lấy từ chức năng Thông Tin, Thay Đổi Số Loại Tiền Công.\r\n"
+                    + "\t- Thành tiền: Dữ liệu được tính tự động theo công thức (số lượng x đơn giá + tiền công).\r\n"
+                    + "\t- Nút thêm sẽ đưa dữ liệu đã nhập hợp lệ vào bản bên trái.\r\n"
+                    + "\t- Nút hủy sẽ xóa hết các dữ liệu đã nhập ở khu vực thao tác.\r\n\r\n"
+                    + "\tPhía bên trái là khu vực xử lí các thông tin tổng quan của phiếu sửa chữa:\r\n"
+                    + "\t- Số phiếu: Chuỗi kí tự.\r\n"
+                    + "\t- Biển số: Dữ liệu được lấy từ chức năng Tra Cứu Xe.\r\n"
+                    + "\t- Bảng thể hiện các thông tin của phiếu đang nhập.\r\n"
+                    + "\t- Nút Tạo Phiếu Mới: Xóa hết các dữ liệu của phiếu đang nhập.\r\n"
+                    + "\t- Nút Lập Phiếu: Nhập thông tin của phiếu vào khi đủ các dữ liệu.\r\n"
+                    + "\t- Nút Lịch Sử Lập Phiếu: Hiển thị thông tin của các phiếu đã nhập.\r\n"
+                    + "\t- Nút Xuất File: Xuất nội dung của phiếu đang nhập ra tập tin Excel.";
 			}
 			if (selectedNodeText == "Lập phiếu thu tiền")
 			{
-				tbHelp.Text = "Hướng dẫn lập phiếu thu tiền.";
+				tbHelp.Text = "\tDữ liệu ô Biển Số được lấy từ chức năng Tra Cứu Xe.\r\n"
+                    + "\tDữ liệu ô Tên Chủ Xe được lấy từ chức năng Quản Lý Khách Hàng.\r\n"
+                    + "\tÔ Số Tiền Nợ và Số Tiền Khách Trả chỉ nhập vào được các dạng số.\r\n"
+                    + "\tÔ Số Tiền Trả Khách và Số Tiền Thu được tính tự động.\r\n"
+                    + "\tNút Lập Phiếu sẽ nhập dữ liệu vào khi điền đầy đủ và hợp lệ các thông tin.\r\n"
+                    + "\tNút Lịch Sử Thu Tiền sẽ xuất kết quả các phiếu đã nhập ở bảng bên dưới.";
 			}
-			if (selectedNodeText == "Báo cáo tồn")
+            if (selectedNodeText == "Báo cáo")
+            {
+                tbHelp.Text = "\tChức năng báo cáo bao gồm 2 chức năng nhỏ:\r\n"
+                    + "\t- Báo cáo tồn: Lập báo cáo về số lượng tồn cho vật tư phụ tùng trong gara theo tháng.\r\n"
+                    + "\t- Doanh số: Lập báo cáo về doanh số của gara theo tháng.";
+            }
+            if (selectedNodeText == "Báo cáo tồn")
 			{
-				tbHelp.Text = "Hướng dẫn báo cáo tồn.";
+				tbHelp.Text = "\tMã báo cáo tồn nhập vào là chuỗi kí tự.\r\n"
+                    + "\tNút Lập Báo Cáo sẽ tự động lập báo cáo dựa vào các dữ liệu đã nhập về vật tư phụ tùng ở mục Quản Lý Vật Tưhay khi sử dụng trong sửa chữa.\r\n"
+                    + "\tNút Xuất File Excel sẽ xuất kết quả của báo cáo đã hiện về trên bảng ra tập tin Excel.";
 			}
 			if (selectedNodeText == "Doanh số")
 			{
-				tbHelp.Text = "Hướng dẫn doanh số.";
+				tbHelp.Text = "\tMã báo cáo doanh số nhập vào là chuỗi kí tự.\r\n"
+                    + "\tTổng Doanh Thu được tính tự động từ đữ liệu các Phiếu Thu Tiền đã nhập.\r\n"
+                    + "\tNút Lập Báo Cáo sẽ tự động lập báo cáo dựa vào các dữ liệu đã nhập ở mục Giao Dịch, chức năng Phiếu Thu Tiền.\r\n"
+                    + "\tNút Xuất File Excel sẽ xuất kết quả của báo cáo đã hiện về trên bảng ra tập tin Excel.";
 			}
 			if (selectedNodeText == "Thay đổi Database")
 			{
